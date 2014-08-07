22537db437db8e74eec32fcee053400b58b7d926
emmanuel martinez blas <dj.nash.emmanuel@gmail.com>
2014-08-07 09:01:41 -0500

    Version 1
    Generacion de  la interfas de un piano

---
+++ js/acciones.js
@@ -0,0 +1,20 @@
+//JavaScript
+	var audio ;
+$(document).ready(function(e){
+	$("#principal").height($("#page").height())
+	document.addEventListener("deviceready",function(){
+		audio=window.plugins.LowLatencyAudio;
+		
+		audio.preloadFX('re','audio/D.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('do','audio/C.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('mi','audio/E.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('fa','audio/F.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('sol','audio/G.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('la','audio/A.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		audio.preloadFX('si','audio/B.mp3',function(msg){},function(msg){alert ('error: '+msg);});
+		
+		
+	
+	
+	});//ready
+	});//ready
